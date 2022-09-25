4e694286c1052fdf57035be03c58319f2c60edab
Adrian González <adrigondo.profesional@gmail.com>
2022-09-25 13:46:34 -0600

    Update: Imports of graphql and mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      // PRODUCTION MODE:
+      // debug: false,
+      // playground: false,
+    }),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+
+
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

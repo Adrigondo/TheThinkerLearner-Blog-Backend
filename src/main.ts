75633989833ba4dd410779cec372206036f788c8
Adrian González <adrigondo.profesional@gmail.com>
2022-09-27 12:59:23 -0600

    Update: trying repare heroku deploy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+    console.log("Server is running.");
+  });
 }
 bootstrap();

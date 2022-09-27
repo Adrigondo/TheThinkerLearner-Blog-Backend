9eaea62c0190e9e3e16b7c8369d13c55342b175a
Adrian González <adrigondo.profesional@gmail.com>
2022-09-27 16:25:27 -0600

    Update: Heroku try 1

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
-    console.log("Server is running.");
-  });
+  // app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();

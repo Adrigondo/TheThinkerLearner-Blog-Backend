546b3a8948917be0b43ba0ad3a3d866a77587b12
Adrian González <adrigondo.profesional@gmail.com>
2022-10-09 17:15:24 -0600

    Try to fix backend 1.1

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+    console.log("Server is running.");
+  });
 }
 bootstrap();
